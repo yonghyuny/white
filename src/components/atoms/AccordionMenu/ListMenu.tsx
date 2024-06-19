b8ac49f7fadcd0df129c062e577730da17041812
김용현 <lovelove4695@gmail.com>
2024-06-20 08:56:24 +0900

    third commit

--- src/components/atoms/AccordionMenu/ListMenu.tsx
+++ src/components/atoms/AccordionMenu/ListMenu.tsx
@@ -5,36 +5,39 @@ import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import { useRouter } from "next/router";
 
 export type ListMenuItem = {
-    name:string;
-    url:string;
-    
-}
+  name: string;
+  url: string;
+};
 
 export type ListMenuItemProps = {
-    ListMenuItems:ListMenuItem[]
-}
+  ListMenuItems: ListMenuItem[];
+};
 
-const ListMenu = ({ListMenuItems}:ListMenuItemProps) => {
-    const router = useRouter()
-    const moveToUrl = (url:string) => router.push(`/${url}`)
+const ListMenu = ({ ListMenuItems }: ListMenuItemProps) => {
+  const router = useRouter();
+  const moveToUrl = (url: string) => router.push(`/${url}`);
 
-    return (
-       <>
-        {ListMenuItems?.map((value) => (
-            <ListItem key={value.name}
-                      onClick={() => moveToUrl(value.url)}
-                      sx={{
-                          borderColor: 'divider', transition: "all 0.3s",
-                      }}
-            >
-                <ListItemButton>
-                    <ListItemText primaryTypographyProps={{fontSize: SIZES.FONT_SIZE.SMALL}}
-                                  primary={value.name}/>
-                </ListItemButton>
-            </ListItem>
-        ))}
+  return (
+    <>
+      {ListMenuItems?.map((value) => (
+        <ListItem
+          key={value.name}
+          onClick={() => moveToUrl(value.url)}
+          sx={{
+            borderColor: "divider",
+            transition: "all 0.3s",
+          }}
+        >
+          <ListItemButton>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: SIZES.FONT_SIZE.SMALL }}
+              primary={value.name}
+            />
+          </ListItemButton>
+        </ListItem>
+      ))}
     </>
-    )
-}
+  );
+};
 
-export default ListMenu
+export default ListMenu;
